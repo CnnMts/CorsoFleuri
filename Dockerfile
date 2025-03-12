0d80ab6caaf21fc698e29688e094205e0b73b73e
Matis Conan <matisconan2006@gmail.com>
2025-03-12 10:47:36 +0100

    Server node with php

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM node:20.18.2
-WORKDIR /www
+WORKDIR /src
 COPY package*.json ./
 RUN npm install
 COPY . .
-EXPOSE 8081
-CMD ["node", "server.js"]
+EXPOSE 8082
+CMD ["npm", "run", "dev"]
--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 const express = require('express');
-
+const axios = require('axios');
 const app = express();
+const port = 8082;
 
-app.get('/',(req, res) =>{
-  res.send("Hello Word ! ");
-})
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
-app.listen(8081);
+app.get('/php', async (req, res) => {
+  try {
+    console.log('Sending request to PHP server...');
+    const response = await axios.get('http://nginx:8083/index.php');
+    console.log('Response from PHP server:', response.data);
+    res.send(response.data);
+  } catch (error) {
+    if (error.response) {
+      console.error('Error setting up the request:', error.message);
+      res.status(500).send('Error setting up the request');
+    }
+  }
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
