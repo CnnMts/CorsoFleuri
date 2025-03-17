59d8d2c0161e921d15c7685758848f5fbb8dc2d3
Matis <matisconan2006@gmail.com>
2025-03-17 14:41:04 +0100

    Start front

---
+++ Front/src/Models/getProduct.js
@@ -0,0 +1,16 @@
+
+async function fetchProducts() {
+  try {
+    const response = await fetch('http://localhost:8083/product');
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const products = await response.json();
+    console.log('Produits récupérés:', products);
+    // Vous pouvez maintenant utiliser les produits dans votre application
+  } catch (error) {
+    console.error('Erreur lors de la récupération des produits:', error);
+  }
+}
+
+fetchProducts();
