a366aa538459a9aea5067f24c810194625573865
Azhega <lethan986@gmail.com>
2025-03-17 14:13:46 +0100

    feat : [back] Double Nginx config for back and front + some table CRUDs

--- app/src/controllers/OrderMenu.php
+++ app/src/controllers/OrderMenu.php
@@ -3,49 +3,49 @@
 namespace App\Controllers;
 
 use App\Controllers\Controller;
-use App\Models\OrderItemModel;
+use App\Models\OrderMenuModel;
 use App\Utils\{Route,HttpException};
 // use App\Middlewares\AuthMiddleware;
 // use App\Middlewares\RoleMiddleware;
 
-class OrderItem extends Controller {
-  protected object $orderItem;
+class OrderMenu extends Controller {
+  protected object $orderMenu;
 
   public function __construct($param) {
-    $this->orderItem = new OrderItemModel();
+    $this->orderMenu = new OrderMenuModel();
 
     parent::__construct($param);
   }
 
   /*========================= POST ==========================================*/
 
-  #[Route("POST", "/orderItem")]
-  public function createOrderItem() {
-    $this->orderItem->add($this->body);
+  #[Route("POST", "/orderMenu")]
+  public function createOrderMenu() {
+    $this->orderMenu->add($this->body);
 
-    return $this->orderItem->getLast();
+    return $this->orderMenu->getLast();
   }
 
   /*========================= GET BY ID =====================================*/
 
-  #[Route("GET", "/orderItem/:id")] 
-  public function getOrderItem() {
-    return $this->orderItem->get(intval($this->params['id']));
+  #[Route("GET", "/orderMenu/:id")] 
+  public function getOrderMenu() {
+    return $this->orderMenu->get(intval($this->params['id']));
   }
 
   /*========================= GET ALL =====================================*/
 
-  #[Route("GET", "/orderItem")]
-  public function getOrderItems() {
+  #[Route("GET", "/orderMenu")]
+  public function getOrderMenus() {
       $limit = isset($this->params['limit']) ? 
       intval($this->params['limit']) : null;
-      return $this->orderItem->getAll($limit);
+      return $this->orderMenu->getAll($limit);
   }
 
   /*========================= GET BY ID =====================================*/
 
-  #[Route("PATCH", "/orderItem/:id")]
-  public function updateOrderItem() {
+  #[Route("PATCH", "/orderMenu/:id")]
+  public function updateOrderMenu() {
     try {
       $id = intval($this->params['id']);
       $data = $this->body;
@@ -57,16 +57,16 @@ class OrderItem extends Controller {
 
       # Check for missing fields
       $missingFields = array_diff(
-        $this->orderItem->authorized_fields_to_update, array_keys($data));
+        $this->orderMenu->authorized_fields_to_update, array_keys($data));
       if (!empty($missingFields)) {
         throw new HttpException(
           "Missing fields: " . implode(", ", $missingFields), 400);
       }
 
-      $this->orderItem->update($data, intval($id));
+      $this->orderMenu->update($data, intval($id));
 
       # Let's return the updated order
-      return $this->orderItem->get($id);
+      return $this->orderMenu->get($id);
     } catch (HttpException $e) {
       throw $e;
     }
@@ -74,9 +74,9 @@ class OrderItem extends Controller {
 
   /*========================= DELETE ========================================*/
 
-  #[Route("DELETE", "/orderItem/:id")]
-  public function deleteOrderItem() {
-    return $this->orderItem->delete(intval($this->params['id']));
+  #[Route("DELETE", "/orderMenu/:id")]
+  public function deleteOrderMenu() {
+    return $this->orderMenu->delete(intval($this->params['id']));
   }
 }
 
